392cc1aa6d0562eb035ee631d38384b175018a62
Greg Hoelzer <ghoelzer@redhat.com>
2017-01-16 16:12:05 -0600

    initial base docker pull scripts

---
+++ scripts/docker_pull_OCP33base.sh
@@ -0,0 +1,6 @@
+docker pull registry.access.redhat.com/openshift3/ose-haproxy-router:v3.3.1.7
+docker pull registry.access.redhat.com/openshift3/ose-deployer:v3.3.1.7
+docker pull registry.access.redhat.com/openshift3/ose-sti-builder:v3.3.1.7
+docker pull registry.access.redhat.com/openshift3/ose-docker-builder:v3.3.1.7
+docker pull registry.access.redhat.com/openshift3/ose-pod:v3.3.1.7
+docker pull registry.access.redhat.com/openshift3/ose-docker-registry:v3.3.1.7
